462057a70111ca697ac18a68ab085295f64712c0
David <davidspeers25@outlook.com>
2017-11-06 15:36:10 +0000

    Added working SplashScreen

---
+++ app/src/main/java/com/example/david/songlek/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.david.songlek
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val background = object : Thread() {
+            override fun run() {
+                Thread.sleep((3000).toLong())
+
+                startActivity(Intent(baseContext, MainActivity::class.java))
+                finish()
+            }
+
+        }
+        background.start()
+    }
+}
