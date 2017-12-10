c8f3c3727b4d800829c3ba61c02802e82f49f027
David <davidspeers25@outlook.com>
2017-12-10 04:40:23 +0000

    Created Basic UnlockedSongs ListView

--- app/src/main/java/com/example/david/songlek/UnlockedSongsActivity.kt
+++ app/src/main/java/com/example/david/songlek/UnlockedSongsActivity.kt
@@ -1,10 +1,8 @@
 package com.example.david.songlek
 
-import android.content.Context
+/*import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ListView
-import org.jetbrains.anko.*
 
 class UnlockedSongsActivity : AppCompatActivity() {
 
@@ -24,5 +22,60 @@ class UnlockedSongsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_unlocked_songs)
 
         getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+    }
+}*/
+import android.content.Context
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log
+import org.jetbrains.anko.*;
+import java.util.*
+
+class UnlockedSongsActivity : AppCompatActivity() {
+
+    val task_list = ArrayList<String>()         //list consisting of tasks
+    var title = ""
+    var artist = ""
+    var link = ""
+
+    val PREFS_FILE = "MyPrefsFile" // for storing preferences
+    private var unlockedSongNumbers = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        doAsync {
+            DownloadXmlTask().execute("http://www.inf.ed.ac.uk/teaching/courses/cslp/data/songs/songs.xml")
+            uiThread {
+                val settings = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE)
+                unlockedSongNumbers = settings.getString("unlockedSongNumbers", "")
+                Log.v("unlockedSongs", unlockedSongNumbers)
+                val unlockedSongsList = unlockedSongNumbers.split(",")
+                for (songNumber in unlockedSongsList) {
+                    //add if songslist < greatest number
+                    if (songNumber == "") {
+                        //Do Nothing
+                    } else {
+                        task_list.add(songsList[songNumber.toInt()].title)
+                        Log.v("unlockedSongs", songsList[songNumber.toInt()].title)
+                    }
+                }
+            }
+        }
+
+
+
+        savedInstanceState?.let {
+            val arrayList = savedInstanceState.get("ToDoList")
+            task_list.addAll(arrayList as List<String>)
+        }
+        var adapter=UnlockedSongsAdapter(task_list)      //define adapter
+        var ui = UnlockedSongsUI(adapter)                //define Anko UI Layout to be used
+        ui.setContentView(this)                 //Set Anko UI to this Activity
+
+    }
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState?.putStringArrayList("ToDoList", task_list)
+        super.onSaveInstanceState(outState)
     }
 }
---
+++ app/src/main/java/com/example/david/songlek/UnlockedSongsUI.kt
@@ -0,0 +1,42 @@
+package com.example.david.songlek
+
+/**
+ * Created by David on 10/12/2017.
+ */
+import android.content.Intent
+import android.net.Uri
+import android.support.v4.content.ContextCompat
+import android.view.Gravity
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ListView
+import org.jetbrains.anko.*
+import org.jetbrains.anko.appcompat.v7.toolbar
+import org.jetbrains.anko.design.floatingActionButton
+
+class UnlockedSongsUI(val UnlockedSongsAdapter: UnlockedSongsAdapter) : AnkoComponent<UnlockedSongsActivity> {
+    override fun createView(ui: AnkoContext<UnlockedSongsActivity>): View = with(ui) {
+        return relativeLayout {
+            var todoList : ListView? =null
+
+
+            //layout to display ListView
+            verticalLayout {
+                todoList=listView {
+                    adapter = UnlockedSongsAdapter
+                    onItemClick { adapterView, view, i, l ->
+                        //startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(UnlockedSongsAdapter.getItem(i).link)))
+                        true
+                    }
+                }
+            }.lparams {
+                margin = dip(5)
+            }
+
+        }
+
+    }
+
+    //function to get total number of items in list
+    fun getTotalListItems(list: ListView?) = list?.adapter?.count ?: 0
+}
